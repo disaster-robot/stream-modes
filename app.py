b49eaafcda48a3e21e8f5676618719c21b37a6fc
Disaster Robot <disaster_robot@protonmail.com>
2021-08-16 22:42:51 -0400

    Create stubbed widget endpoint.

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import (
     Flask, render_template
 )
 
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("status_line.html")
+
+@app.route("/widgets/<widget_uuid>")
+def widget(widget_uuid):
+    return json.dumps({ "left": "timer", "center": "text", "right": "vibe mode" })
