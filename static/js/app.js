9fa119289b78005be0a8f9788d865a203d95cc62
Disaster Robot <disaster_robot@protonmail.com>
2021-08-16 22:53:56 -0400

    make an ajax request

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,10 @@
-var hello = "world";
+var httpRequest = new XMLHttpRequest();
+
+function updateWidget() {
+    console.log('updateWidget');
+}
+
+httpRequest.onreadystatechange = updateWidget;
+
+httpRequest.open('GET', 'http://localhost:5000/widgets/auuid', true);
+httpRequest.send();
